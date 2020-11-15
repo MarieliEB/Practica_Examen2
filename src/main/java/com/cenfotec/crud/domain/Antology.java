eb34b0ba02e4ef64410af56d420e4842cf99c25c
eespinozab <mespinozab@ucenfotec.ac.cr>
2020-11-15 17:50:10 -0600

    practica de examen

---
+++ src/main/java/com/cenfotec/crud/domain/Antology.java
@@ -0,0 +1,70 @@
+package com.cenfotec.crud.domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Antology {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;	
+	private Date date;
+	private String text;
+	
+	private String name;
+	
+	@OneToMany(fetch=FetchType.LAZY, mappedBy="anthology")
+	private Set<Article> articles;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Set<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(Set<Article> articles) {
+		this.articles = articles;
+	}
+	
+	
+}
